447db2c17e51d3c8567832e170e29a30756092fe
Darren Henderson <darrenhusa@yahoo.com>
2021-05-17 09:43:15 -0500

    Refactor nav-link blade component. Add dropdown-link blade component.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -32,12 +32,12 @@
         </button>
         </div>
         <nav :class="{'block': open, 'hidden': !open}" class="flex-grow md:block px-4 pb-4 md:pb-0 md:overflow-y-auto">
-        <x-nav-link text="Line Chart" path="/" class="{{ Request::path() == '/' ? 'bg-gray-200' : 'bg-transparent'}}" />
-        <x-nav-link text="Column Chart" path="column_chart" class="{{ Request::path() == 'column_chart' ? 'bg-gray-200' : 'bg-transparent'}}" />
-        <x-nav-link text="Pie Chart" path="pie_chart" class="{{ Request::path() == 'pie_chart' ? 'bg-gray-200' : 'bg-transparent'}}" />
-        <x-nav-link text="Stacked Column Chart Chart" path="stacked_column_chart" class="{{ Request::path() == 'stacked_column_chart' ? 'bg-gray-200' : 'bg-transparent'}}" />            
-        <x-nav-link text="100% Stacked Column Chart" path="100_pct_stacked_column_chart" class="{{ Request::path() == '100_pct_stacked_column_chart' ? 'bg-gray-200' : 'bg-transparent'}}" />            
-        <x-nav-link text="Combo Chart" path="combo_chart" class="{{ Request::path() == 'combo_chart' ? 'bg-gray-200' : 'bg-transparent'}}" />            
+        <x-nav-link href="/" class="{{ Request::path() == '/' ? 'bg-gray-200' : 'bg-transparent'}}">Line Chart</x-nav-link>
+        <x-nav-link href="column_chart" class="{{ Request::path() == 'column_chart' ? 'bg-gray-200' : 'bg-transparent'}}">Column Chart</x-nav-link>
+        <x-nav-link href="pie_chart" class="{{ Request::path() == 'pie_chart' ? 'bg-gray-200' : 'bg-transparent'}}">Pie Chart</x-nav-link>
+        <x-nav-link href="stacked_column_chart" class="{{ Request::path() == 'stacked_column_chart' ? 'bg-gray-200' : 'bg-transparent'}}">Stacked Column Chart Chart</x-nav-link>            
+        <x-nav-link href="100_pct_stacked_column_chart" class="{{ Request::path() == '100_pct_stacked_column_chart' ? 'bg-gray-200' : 'bg-transparent'}}">100% Stacked Column Chart</x-nav-link>                        
+        <x-nav-link href="combo_chart" class="{{ Request::path() == 'combo_chart' ? 'bg-gray-200' : 'bg-transparent'}}">Combo Chart</x-nav-link>                                    
             
             <div @click.away="open = false" class="relative" x-data="{ open: false }">
                 <button @click="open = !open" class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:block hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
@@ -46,7 +46,8 @@
                 </button>
                     <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg">
                     <div class="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">
-                        <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="/">Line Chart</a>
+                        <x-dropdown-link href="/">Line Chart</x-dropdown-link>
+
                         <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="column_chart">Column Chart</a>
                         <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Link #3</a>
                     </div>
