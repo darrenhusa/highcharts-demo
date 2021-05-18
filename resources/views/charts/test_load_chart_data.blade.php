a2bec9cfc2f55cac416f5e52ef1b2be5c3537c70
Darren Henderson <darrenh@ccsj.edu>
2021-05-17 21:09:45 -0500

    Copy and test highchart themes.

--- resources/views/charts/test_load_chart_data.blade.php
+++ resources/views/charts/test_load_chart_data.blade.php
@@ -19,6 +19,7 @@
 @endsection
 
 @section('scripts')
+    <script src="/highcharts/themes/grid-light.js"></script>
     <script> window.count = '<?php echo $jsonResult ?>'; </script>
     <script src="/js/test_load_chart_data.js"></script>
 @endsection
